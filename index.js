5f3f140d323f5a117c2ee8ef113f7100498cba68
victor <victor.buthia@student.moringaschool.com>
2022-11-29 12:45:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  console.log(leftNumbers)
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
